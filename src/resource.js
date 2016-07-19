ffe42c6193c8efa76d612fa28f2df3c78a7385b9
syunsuke takahshi <syunsuke231@gmail.com>
2016-07-19 13:29:02 +0900

    resource.jsの更新

--- src/resource.js
+++ src/resource.js
@@ -1,8 +1,8 @@
 var res = {
     background_png : "res/background.png",
-    ship_png : "res/ship.png",
-    particle_png : "res/particle.png",
-    asteroid_png : "res/asteroid.png",
+    ship_png : "res/shrimp03.png",
+    particle_png : "res/wave.png",
+    asteroid_png : "res/nagoya0.png",
     bgm_main : "res/bgm_piano09.mp3",
     se_bang : "res/se_bang.mp3"
 };
